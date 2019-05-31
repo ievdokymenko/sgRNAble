024aef99ff8bdc64cc04a3d0fedb505ff1115215
Siddarth <sidr97@gmail.com>
2019-05-31 16:21:52 -0700

    Methionine Modifications
    Changed the program to allow the presence of Mets to determine which guides to show and which ones not to show. In addition, quality of life improvements such as a progress bar, Azimuth Score in the excel file, and bug fixes.

--- Azimuth_Finder.py
+++ Azimuth_Finder.py
@@ -43,80 +43,86 @@ def PAM_Finder(Sequence, PAM, Direction, Cutoff):
   predictions = model_comparison.predict(np.array(Guide_RNAs))
   #Sorting the Guides based on the Azimuth scores and then only selecting the
   #top number of guides as defined by the Azimuth Cutoff
-  Guide_RNAs = [x for y,x in sorted(zip(predictions,Guide_RNAs))][:Azimuth_Cutoff]
-  Location = [x for y,x in sorted(zip(predictions,Location))][:Azimuth_Cutoff]
-  Strand = [x for y,x in sorted(zip(predictions,Strand))][:Azimuth_Cutoff]
+  Guide_RNAs = [x for y,x in sorted(zip(predictions,Guide_RNAs), reverse=True)][:Azimuth_Cutoff]
+  Location = [x for y,x in sorted(zip(predictions,Location), reverse=True)][:Azimuth_Cutoff]
+  Strand = [x for y,x in sorted(zip(predictions,Strand), reverse=True)][:Azimuth_Cutoff]
+  Azimuth_Score = sorted(predictions, reverse=True)[:Azimuth_Cutoff]
+
 
   #Trimming the extra basepairs need for the Azimuth Model from the Guide RNAs
   #so that they fit into the Salis Lab's model.
   for i in range(len(Guide_RNAs)):
       Guide_RNAs[i] = Guide_RNAs[i][4:24]
 
-  return Guide_RNAs,Location,Strand
+  return Guide_RNAs,Location,Strand, Azimuth_Score
 
 def Azimuth_Guides(Gene_Seq, PAM_Seq, Guides_Cutoff, Usage):
 
     if (Usage == "CRISPRi Screening"):
         Position = []
         Direction = []
+        Azimuth_Rank = []
         Contig_Name = []
         Guides = []
         Frames = [1,2,3,-1,-2,-3]
         Stop_Codons = ["TAG","TGA","TAA"]
         Met = "ATG"
-        Reverse_Met = "TAC"
         Start_Codons = [0]
         Reverse_Start_Condons = [0]
-        ORFs = []
+        Complete_ORFs = []
         ORF_Cutoff = 1000
         Location_in_Contig = 0
         Distance_to_Nearest_Met = []
+        Guides_Cutoff = 30
 
-        Guides_Cutoff = 10
-
-        for Contig in Gene_Seq:
+        for k,Contig in enumerate(Gene_Seq):
+            print("Looking for ORFs in")
+            print(Contig.id)
+            print("[", end ="")
+            Contig_ORFs = []
             for Frame in Frames:
                 Contig_Frame = Contig[abs(Frame)-1:]
                 if Frame < 0:
                     Contig_Frame = Contig[abs(Frame)-1:].reverse_complement()
+                print("#", end ="", flush = True)
                 for i in range(len(Contig_Frame)):
                     if str(Contig_Frame[i*3:i*3+3].seq) in Met:
                         Start_Codons.append(i*3)
 
-                    if str(Contig_Frame[i*3:i*3+3].seq) in Reverse_Met:
-                        Reverse_Start_Condons.append(i*3)
-
                     if Contig_Frame[i*3:(i*3)+3].seq in Stop_Codons:
 
                         if(i*3-Location_in_Contig) > ORF_Cutoff and Location_in_Contig != 0:
-                            Gene1, Location1, Direction1 = PAM_Finder(Contig_Frame[Location_in_Contig:(i*3)+3].seq, PAM_Seq, 1,Guides_Cutoff)
+                            Gene1, Location1, Direction1, Azimuth_Score1 = PAM_Finder(Contig_Frame[Location_in_Contig:(i*3)+3].seq, PAM_Seq, 1,Guides_Cutoff)
                             Position.append(Location1)
                             Direction.append(Direction1)
+                            Azimuth_Rank.append(Azimuth_Score1)
                             Meth_Distances = []
                             for Loc in Location1:
-                                if Frame > 0:
-                                    Meth_Distances.append(ClosestDiff(Start_Codons,Loc+Location_in_Contig))
-                                else:
-                                    Meth_Distances.append(ClosestDiff(Reverse_Start_Condons,Loc+Location_in_Contig))
+                                Meth_Distances.append(ClosestDiff(Start_Codons,Loc+Location_in_Contig))
                             Distance_to_Nearest_Met.append(Meth_Distances)
                             Guides.append(Gene1)
-                            ORFs.append(Contig_Frame[Location_in_Contig:(i*3)+3].seq)
+                            Contig_ORFs.append(Contig_Frame[Location_in_Contig:(i*3)+3].seq)
                         Location_in_Contig = i*3
                 Location_in_Contig = 0
             Contig_Name.append(Contig.id)
+            print("]")
+            print("Number of ORFs Found:", len(Contig_ORFs))
+            if len(Contig_ORFs) == 0:
+                Complete_ORFs.append(0)
+            Complete_ORFs.append(Contig_ORFs)
 
-        return Guides, Position, Direction, Contig_Name, Distance_to_Nearest_Met, ORFs
+        return Guides, Position, Direction, Contig_Name, Distance_to_Nearest_Met, Complete_ORFs, Azimuth_Rank
 
     elif (Usage == "CRISPRi on a Gene"):
         for Gene in Gene_Seq:
-            Gene,Location,Direction = PAM_Finder(Gene.seq.upper(), PAM_Seq, -1,Guides_Cutoff)
+            Gene,Location,Direction, Azimuth_Score = PAM_Finder(Gene.seq.upper(), PAM_Seq, -1,Guides_Cutoff)
 
         return Gene, Location, Direction, 0 ,0, 0
 
     else:
 
-        Gene1,Location1,Direction1 = PAM_Finder(Gene_Seq, PAM_Seq, 1,Guides_Cutoff)
-        Gene2,Location2,Direction2 = PAM_Finder(Gene_Seq, PAM_Seq, -1,Guides_Cutoff)
+        Gene1,Location1,Direction1, Azimuth_Score1 = PAM_Finder(Gene_Seq, PAM_Seq, 1,Guides_Cutoff)
+        Gene2,Location2,Direction2, Azimuth_Score2 = PAM_Finder(Gene_Seq, PAM_Seq, -1,Guides_Cutoff)
 
         for i in range(len(Location1)):
             if (Location1[i] > Usage):
@@ -141,9 +147,9 @@ def Azimuth_Guides(Gene_Seq, PAM_Seq, Guides_Cutoff, Usage):
 def ClosestDiff(myList, myNumber):
     pos = bisect_left(myList, myNumber)
     if pos == 0:
-        return ["NA",(myNumber - myList[pos + 1])]
+        return ["NA",(myList[pos + 1])- myNumber]
     if pos == len(myList):
-        return [(myNumber - myList[pos - 1 ]),"NA"]
+        return [(myList[pos - 1 ] - myNumber),"NA"]
     before = myList[pos - 1]
     after = myList[pos]
-    return [(myNumber - before),(myNumber - after)]
+    return [(before - myNumber),(after - myNumber)]
--- Optimal_Guide.py
+++ Optimal_Guide.py
@@ -7,81 +7,108 @@ from Bio.Seq import Seq
 from Cas9_Calculator import *
 from File_GUI import Get_Sequence
 
-#Length of the Guide RNA desired
-Guide_RNA_length = 20
+if __name__ == "__main__":
 
-#Combine the Coding and Template Strands into a single strand
-def CombinetoStr (Template_Guides, Coding_Guides):
-  Guides = []
+    #Length of the Guide RNA desired
+    Guide_RNA_length = 20
 
-  for i in range (len(Template_Guides)):
-    if (i < len(Template_Guides)):
-      Guides.append(Template_Guides[i])
+    #Combine the Coding and Template Strands into a single strand
+    def CombinetoStr (Template_Guides, Coding_Guides):
+      Guides = []
 
-  for i in range (len(Coding_Guides)):
-    if (i < len(Coding_Guides)):
-      Guides.append(Coding_Guides[i])
+      for i in range (len(Template_Guides)):
+        if (i < len(Template_Guides)):
+          Guides.append(Template_Guides[i])
 
-  return Guides
+      for i in range (len(Coding_Guides)):
+        if (i < len(Coding_Guides)):
+          Guides.append(Coding_Guides[i])
 
-def ReverseComplement(nucleotide_sequence):
-  comp = []
-  for c in nucleotide_sequence:
-    if c == 'A' or c == 'a':
-      comp.append('T')
-    if c == 'G' or c == 'g':
-      comp.append('C')
-    if c == 'U' or c == 'u' or c == 'T' or c == 't':
-      comp.append('A')
-    if c == 'C' or c == 'c':
-      comp.append('G')
-  rev_comp = ''.join(reversed(comp))
-  return rev_comp
+      return Guides
 
-Target_Seqs, Genome, Usage = Get_Sequence()
+    def ReverseComplement(nucleotide_sequence):
+      comp = []
+      for c in nucleotide_sequence:
+        if c == 'A' or c == 'a':
+          comp.append('T')
+        if c == 'G' or c == 'g':
+          comp.append('C')
+        if c == 'U' or c == 'u' or c == 'T' or c == 't':
+          comp.append('A')
+        if c == 'C' or c == 'c':
+          comp.append('G')
+      rev_comp = ''.join(reversed(comp))
+      return rev_comp
 
-#Obtain the Guide RNAs from the Target Sequence
-from Azimuth_Finder import * #placed here to avoid multithreading issues with Tkinter (I think)
-PAM = "GG"
-Guide_Num_Cutoff = 20 # Cutoff per strand
+    Target_Seqs, Genome, Usage = Get_Sequence()
 
-Target_Guides, Position_List, Direction_List, Con_Ids, Met_Distance, ORF = Azimuth_Guides(Target_Seqs, PAM,Guide_Num_Cutoff, Usage )
+    #Obtain the Guide RNAs from the Target Sequence
+    from Azimuth_Finder import * #placed here to avoid multithreading issues with Tkinter (I think)
+    PAM = "GG"
+    Guide_Num_Cutoff = 20 # Cutoff per strand
 
-if Usage == "CRISPRi Screening":
-    workbook = xlsxwriter.Workbook('Guide RNAs.xlsx')
-    worksheet = workbook.add_worksheet()
-    row = 0
-    col = 0
-    for Name in Con_Ids:
-        worksheet.write(row,col, "Contig ID:")
-        worksheet.write(row,col + 1 , Name)
-        for i in range(len(ORF)):
-            worksheet.write(row + 1,col + 1 , "ORF Sequence: ")
-            worksheet.write(row + 1,col + 9, str(ORF[i]))
-            for j in range(len(Target_Guides[i])):
-                worksheet.write(row + 2, col +2, "Guide:")
-                worksheet.write(row + 2, col +3 , Target_Guides[i][j])
-                worksheet.write(row + 2, col +4, "Position: ")
-                worksheet.write(row + 2, col +5, Position_List[i][j])
-                worksheet.write(row + 2, col +6, "Strand: ")
-                worksheet.write(row + 2, col +7, Direction_List[i][j])
-                worksheet.write(row + 2, col +8, "Distance to Previous Methanine")
-                worksheet.write(row + 2, col +9, Met_Distance[i][j][0])
-                worksheet.write(row + 2, col +10, "Distance to Next Methanine")
-                worksheet.write(row + 2, col +11, Met_Distance[i][j][1])
-                row += 2
-        row += 2
+    Target_Guides, Position_List, Direction_List, Con_Ids, Met_Distance, Con_ORFs, Azimuth_Score = Azimuth_Guides(Target_Seqs, PAM,Guide_Num_Cutoff, Usage )
 
-    workbook.close()
+    if Usage == "CRISPRi Screening":
 
-else:
-    Genome = Genome.upper() + Genome.upper().reverse_complement()
-    SeqIO.write(Genome, "Total_Genome_Plus_RC", "fasta")
+        Guide_Cutoff = 5
+        workbook = xlsxwriter.Workbook('Guide RNAs.xlsx')
+        worksheet = workbook.add_worksheet()
+        row = 0
+        col = 0
+        ORF_Num = 0
+        Maximum_Met_Distance = 500
+        Minimum_Gene_Distance = 600
+        for k in range(len(Con_Ids)):
+            worksheet.write(row,col, "Contig ID:")
+            worksheet.write(row,col + 1 , Con_Ids[k])
+            if (Con_ORFs[k] == 0):
+                continue
+            for i, ORF in enumerate(Con_ORFs[k]):
+                worksheet.write(row + 1,col + 1 , "ORF Length: ")
+                worksheet.write(row + 1,col + 2, len(ORF))
+                worksheet.write(row + 1,col + 3 , "ORF Sequence: ")
+                worksheet.write(row + 1,col + 10, str(ORF))
+                Guides_Listed = 0
+                print("Target_Guides[ORF_Num + i]")
+                print(Target_Guides[ORF_Num + i])
+                for j in range(len(Target_Guides[ORF_Num + i])):
+                    if (- Met_Distance[i][j][0] > Position_List[i][j]):
+                        continue
+                    if (- Met_Distance[i][j][0] > Maximum_Met_Distance):
+                        continue
+                    if (len(ORF) + Met_Distance[i][j][0] < Minimum_Gene_Distance):
+                        continue
+                    if(Guides_Listed > Guide_Cutoff):
+                        break
+                    else:
+                        Guides_Listed += 1
+                    worksheet.write(row + 2, col +2, "Guide:")
+                    worksheet.write(row + 2, col +3 , Target_Guides[ORF_Num + i][j])
+                    worksheet.write(row + 2, col +4, "Position: ")
+                    worksheet.write(row + 2, col +5, Position_List[ORF_Num + i][j])
+                    worksheet.write(row + 2, col +6, "Strand: ")
+                    worksheet.write(row + 2, col +7, Direction_List[ORF_Num + i][j])
+                    worksheet.write(row + 2, col +8, "Distance to Previous Methionine (bp)")
+                    worksheet.write(row + 2, col +9, Met_Distance[ORF_Num + i][j][0])
+                    worksheet.write(row + 2, col +10, "Distance to Next Methionine (bp)")
+                    worksheet.write(row + 2, col +11, Met_Distance[ORF_Num + i][j][1])
+                    worksheet.write(row + 2, col +12, "Azimuth Model Score")
+                    worksheet.write(row + 2, col +13, Azimuth_Score[ORF_Num + i][j])
+                    row += 2
+            ORF_Num += i
+            row += 2
 
-    #Combine the information from the Guide RNAs into one single array.
-    Guide_Info = np.vstack((Position_List, Direction_List)).T
+        workbook.close()
 
-    #Send Data to the model
-    Cas9Calculator=clCas9Calculator(['Total_Genome_Plus_RC'])
-    sgRNA1 = sgRNA(Target_Guides,Guide_Info, Cas9Calculator)
-    sgRNA1.run()
+    else:
+        Genome = Genome.upper() + Genome.upper().reverse_complement()
+        SeqIO.write(Genome, "Total_Genome_Plus_RC", "fasta")
+
+        #Combine the information from the Guide RNAs into one single array.
+        Guide_Info = np.vstack((Position_List, Direction_List)).T
+
+        #Send Data to the model
+        Cas9Calculator=clCas9Calculator(['Total_Genome_Plus_RC'])
+        sgRNA1 = sgRNA(Target_Guides,Guide_Info, Cas9Calculator)
+        sgRNA1.run()
